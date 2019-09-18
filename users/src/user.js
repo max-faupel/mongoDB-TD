47f7b43dde3c55c96304478630bd9178457a83ef
Maximilian Faupel <max.faupel@gmx.de>
2019-09-18 18:48:44 +0200

    handle subdocuments

--- users/src/user.js
+++ users/src/user.js
@@ -1,16 +1,22 @@
 const mongoose = require('mongoose');
+const PostSchema = require('./post');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   name: {
     type: String,
     validate: {
-      validator: (name) => name.length > 2, 
+      validator: (name) => name.length > 2,
       message: 'Name must be longer than 2 characters.'
     },
     required: [true, 'Name is required.']
   },
-  postCount: Number
+  posts: [PostSchema],
+  likes: Number
+});
+
+UserSchema.virtual('postCount').get(function() {
+  return this.posts.length;
 });
 
 const User = mongoose.model('user', UserSchema);
